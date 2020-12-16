96b7c323c5b46e98f47e91bc9ead11875552a1bb
Jiuchenm <Jiuchen.Miao@outlook.com>
2020-12-16 16:16:20 +0800

    first commit

---
+++ ClientFx/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using ClientFx.ServiceReference;
+using System;
+
+namespace ClientFx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorClient client = new CalculatorClient();
+
+            var result =  client.Add(100.0, 200.0);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using ContractFx;
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri bindingAddress = new Uri("http://localhost:8899/helloworld");
+
+            ServiceHost host = new ServiceHost(typeof(CalculatorService), bindingAddress);
+            try
+            {
+                host.AddServiceEndpoint(typeof(ICalculator), new BasicHttpBinding(), "CalculatorService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(smb);
+
+                host.Open();
+
+                Console.ReadLine();
+
+                host.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception occurred: {0}", e.Message);
+                host.Abort();
+            }
+        }
+    }
+}
